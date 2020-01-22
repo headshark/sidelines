829a23289df3deabf31df425dae57d94ef41dd0d
Patrick <torralbapatrick15@gmail.com>
2020-01-22 17:17:49 -0600

    Add database connection

--- includes/config.php
+++ includes/config.php
@@ -2,4 +2,11 @@
 ob_start(); // Turns on output buffering
 session_start();
 date_default_timezone_set("America/Chicago");
+
+try {
+    $conn = new PDO("mysql:dbname=sidelines;host=localhost", "root", "");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+} catch (PDOException $e) {
+    exit("Connection failed: " . $e->getMessage());
+}
 ?>
