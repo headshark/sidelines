135f80894307be3ed9d1dec9f5a78948ee86eaf0
Patrick <torralbapatrick15@gmail.com>
2020-02-06 18:41:24 -0600

    Display movies and tv shows

--- index.php
+++ index.php
@@ -3,4 +3,7 @@ require_once("includes/header.php");
 
 $preview = new PreviewProvider($conn, $userLoggedIn);
 echo $preview->createPreviewVideo(null);
+
+$containers = new CategoryContainers($conn, $userLoggedIn);
+echo $containers->showAllCategories();
 ?>
