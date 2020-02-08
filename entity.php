be281591d8e150f5f11b1cdef3a3ca138328c56c
Patrick <torralbapatrick15@gmail.com>
2020-02-08 15:49:55 -0600

    Add entity page

---
+++ entity.php
@@ -0,0 +1,18 @@
+<?php
+require_once("includes/header.php");
+
+if (!isset($_GET["id"])) {
+    ErrorMessage::show("No ID passed into page");
+}
+$entityId = $_GET["id"];
+$entity = new Entity($conn, $entityId);
+
+$preview = new PreviewProvider($conn, $userLoggedIn);
+echo $preview->createPreviewVideo($entity);
+
+$seasonProvider = new SeasonProvider($conn, $userLoggedIn);
+echo $seasonProvider->create($entity);
+
+$categoryContainers = new CategoryContainers($conn, $userLoggedIn);
+echo $categoryContainers->showCategory($entity->getCategoryId(), "You might also like");
+?>
